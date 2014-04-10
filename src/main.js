bc8209741ff75fa2876c026fd006bd502297d230
Daniel-Xu <xwd.forever@gmail.com>
2014-04-10 16:50:27 -0400

    fix #4
    add event listener
    using underscore for each

--- src/main.js
+++ src/main.js
@@ -15,5 +15,10 @@ require(['operation', 'underscore'], function(operation, _){
 
     operation.newGame()
     var cells = document.getElementsByClassName('handle')
-    
+
+    _.each(cells, function(item, num){
+        item.addEventListener("click", function(){
+            operation.move(this)
+        })  
+    })
 });
