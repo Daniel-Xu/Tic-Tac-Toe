3e96613e7daeccae10aadefbe42f15dc0cce14b7
Daniel-Xu <xwd.forever@gmail.com>
2014-04-09 23:57:18 -0400

    add test env

--- src/strategy.js
+++ src/strategy.js
@@ -1,3 +1,29 @@
+
+var game = game || {}
+
+
+var myApp = myApp || {};
+// a convenience function for parsing string namespaces and
+// automatically generating nested namespaces
+function extend( ns, ns_string ) {
+    var parts = ns_string.split('.'),
+        parent = ns,
+        pl, i;
+    if (parts[0] == "myApp") {
+        parts = parts.slice(1);
+    }
+    pl = parts.length;
+    for (i = 0; i < pl; i++) {
+        //create a property if it doesnt exist
+        if (typeof parent[parts[i]] == 'undefined') {
+            parent[parts[i]] = {};
+        }
+        parent = parent[parts[i]];
+    }
+    return parent;
+}
+
+
 var strategy = (function(){
     var turn = -1,
         tie = 0,
