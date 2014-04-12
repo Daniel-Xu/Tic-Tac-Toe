22a5c1dee3a87be17e4d0491c7f7ac7c59477394
Daniel-Xu <xwd.forever@gmail.com>
2014-04-12 11:08:41 -0400

    add some event comments

--- src/main.js
+++ src/main.js
@@ -12,32 +12,33 @@ require.config({
 });
 
 require(['operation', 'underscore', 'cell_init'], function(operation, _, cellsInit){
-    operation.newGame()
-    var cells = document.getElementsByClassName('handle')
+    //change the text with setting
+    var player1 = document.getElementById('player1')
+    var player2 = document.getElementById('player2')
+    player1.innerHTML = cellsInit.getFirstPlayer()
+    player2.innerHTML = cellsInit.getSecondPlayer()
 
+    //click event
+    var cells = document.getElementsByClassName('handle')
     _.each(cells, function(item, num){
         item.addEventListener("click", function(){
             operation.move(this)
         })  
     })
 
+    //option event
     var players = document.getElementsByClassName("player-option")
-
     _.each(players,function(item, i){
         item.addEventListener("change", function(){
             operation.newGame()
         })
     })
     
+    //new game event
     var newButton = document.getElementById("new-button")
     newButton.addEventListener('click', function(){
         operation.newGame()
     })
 
-
-    var player1 = document.getElementById('player1')
-    var player2 = document.getElementById('player2')
-
-    player1.innerHTML = cellsInit.getFirstPlayer()
-    player2.innerHTML = cellsInit.getSecondPlayer()
+    operation.newGame()
 });
