23692389fcda8fb294b0c4ecb6ec88bc047b6a35
Daniel-Xu <xwd.forever@gmail.com>
2014-04-25 16:59:04 -0400

    board constructor

--- src/board.js
+++ src/board.js
@@ -1,19 +1,77 @@
-define(["underscore", "cell"], function(_, cell){
+define(["underscore"], function(_){
 
     function Board(el) {
         this.element = el
-        this.state = this._getState()
+        this.state = this.getState()
+        this.winner = this.detectWin()
     }
 
-    Board.prototype._getState = function() {
-        var state = [];
-        _.each(this.element, function(cell, i){
-            state.push(cell.element.value)
+    Board.prototype.getState = function() {
+        return _.map(this.element, function(cell, i){
+            return cell.element.value
         })
-
-        return state;
     }
 
-    return Board;
+    Board.prototype.updateState = function(){
+        this.state = this.getState()
+        this.winner = this.detectWin()
+    }
 
+    Board.prototype.isFull = function(){
+        if(_.contains(this.state, ''))
+            return false
+        return true
+    }
+
+    function _isArray(obj){
+        return Object.prototype.toString.call(obj) === '[object Array]'
+    }
+
+    Board.prototype.drawBoard = function(){
+        if(_isArray(this.winner)){
+            _.each(this.winner, function(index){
+                this.element[index].element.style.backgroundColor = 'red'
+            }, this)
+        } 
+        if(this.winner === "tie") alert("It's a tie! Nice try!");
+    }
+
+    Board.prototype.clearUpBoard = function(){
+        _.each(this.element, function(cell){
+            cell.element.value = ""
+            cell.element.style.backgroundColor = '#3498db'
+        })
+    }
+
+
+    Board.prototype.detectWin = function(){
+
+        var wins = [
+            [0, 1, 2], 
+            [3, 4, 5],
+            [6, 7, 8], 
+            [0, 3, 6], 
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        if(this.isFull(this.state))
+            return "tie"
+
+        var winner = _.find(wins, function(situation){
+            return (this.state[situation[0]] === this.state[situation[1]]
+              && this.state[situation[0]] === this.state[situation[2]]
+              && this.state[situation[0]] !== '') 
+        }, this)
+
+        if(winner !== undefined)
+            return winner
+
+        return 0
+    }       
+
+
+    return Board
 })
